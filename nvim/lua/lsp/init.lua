5ca801bd5eacdc19126e27c45b9ebc4a8cbd3320
yzl178me <1403341393@qq.com>
2022-10-10 23:34:11 +0800

    update

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,31 +1,32 @@
 local utils = require("utils")
 
 local lspList = {
-  "cssls",
-  "volar",
-  "sumneko_lua",
-  "gopls",
-  "bashls",
-  "cssmodules_ls",
-  "eslint",
-  "html",
-  "jsonls",
-  "rust_analyzer",
-  "sqls",
-  "clangd",
-  "taplo",
-  "verible",
-  "tailwindcss",
-  -- "pyright",
-  "pylsp",
-  -- "sourcery"
+	"cssls",
+	"volar",
+	"sumneko_lua",
+	"gopls",
+	"bashls",
+	"cssmodules_ls",
+	"eslint",
+	"html",
+	"jsonls",
+	"rust_analyzer",
+	"sqls",
+	"clangd",
+	"taplo",
+	"verible",
+	"tailwindcss",
+	"pyright",
+	-- "jedi_language_server",
+	-- "pylsp",
+	-- "sourcery"
 }
 
 -- common lsp config
 local on_attach = function(client, _)
-  -- disable default format feature of lsp
-  -- client.server_capabilities.document_formatting = false -- 0.7 and earlier
-  -- client.server_capabilities.documentFormattingProvider = false -- 0.8 and hopefully later
+	-- disable default format feature of lsp
+	-- client.server_capabilities.document_formatting = false -- 0.7 and earlier
+	-- client.server_capabilities.documentFormattingProvider = false -- 0.8 and hopefully later
 end
 
 -- nvim-cmp support
@@ -33,33 +34,33 @@ local capabilities = utils.fn.require("cmp_nvim_lsp").update_capabilities(vim.ls
 
 -- load all lsp config
 for _, value in pairs(lspList) do
-  local config = utils.fn.require("lsp/" .. value)
+	local config = utils.fn.require("lsp/" .. value)
 
-  -- set on_attach
-  if config.on_attach == nil then
-    config.on_attach = on_attach
-  else
-    local on_attach_tmp = config.on_attach
-    config.on_attach = function(client, bufnr)
-      on_attach(client, bufnr)
-      on_attach_tmp(client, bufnr)
-    end
-  end
+	-- set on_attach
+	if config.on_attach == nil then
+		config.on_attach = on_attach
+	else
+		local on_attach_tmp = config.on_attach
+		config.on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
+			on_attach_tmp(client, bufnr)
+		end
+	end
 
-  -- set capabilities
-  if value ~= "taplo" then
-    if config.modify_capabilities == nil then
-      config.capabilities = capabilities
-    else
-      config.capabilities = config.modify_capabilities(capabilities)
-    end
-  end
+	-- set capabilities
+	if value ~= "taplo" then
+		if config.modify_capabilities == nil then
+			config.capabilities = capabilities
+		else
+			config.capabilities = config.modify_capabilities(capabilities)
+		end
+	end
 
-  -- print(value,config)
-  -- utils.fn.require("lspconfig")[value].setup(config)
-  -- print(utils.fn.require("lspconfig")[value])
-  local _,err = pcall(utils.fn.require("lspconfig")[value].setup,config)
-  if err ~= nil then
-    print("err",err)
-  end
+	-- print(value,config)
+	-- utils.fn.require("lspconfig")[value].setup(config)
+	-- print(utils.fn.require("lspconfig")[value])
+	local _, err = pcall(utils.fn.require("lspconfig")[value].setup, config)
+	if err ~= nil then
+		print("err", err)
+	end
 end
--- nvim/lua/lsp/pylsp.lua
+++ nvim/lua/lsp/pylsp.lua
@@ -1,4 +1,14 @@
 local M = {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {},
+          maxLineLength = 150
+        }
+      }
+    }
+  }
 }
 
 return M
