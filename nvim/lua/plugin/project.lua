4eb7002301977516ecd86efd6a54376673b6c39b
yzl178me <1403341393@qq.com>
2022-10-24 20:09:25 +0800

    update: porject setting

--- nvim/lua/plugin/project.lua
+++ nvim/lua/plugin/project.lua
@@ -2,7 +2,7 @@ local utils = require("utils")
 
 utils.fn.require("project_nvim").setup({
 	detection_methods = { "pattern" },
-	patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".sln","init.lua","main.py" },
+	patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".sln","init.lua","Pipfile" },
 })
 
 utils.fn.require("telescope").load_extension("projects")
