88f0dfa41ed5d40cf5185182f7889b959d213452
yzl178me <1403341393@qq.com>
2023-05-14 23:31:43 +0800

    nvim: update neorg

--- nvim-lazy/.config/nvim/lua/178me/plugins/neorg.lua
+++ nvim-lazy/.config/nvim/lua/178me/plugins/neorg.lua
@@ -15,124 +15,126 @@
 -- unmap = <function 13>,
 -- unmapd = <function 14>
 local function task_command(command)
-  return "core.norg.qol.todo_items.todo." .. command
+  return "core.qol.todo_items.todo." .. command
 end
 return {
-  "nvim-neorg/neorg",
-  lazy = false,
-  build = ":Neorg sync-parsers",
-  keys = {
-    { "<leader>nl", "<cmd>Neorg<CR>", desc = "list", silent = true },
-    { "<leader>nk", "<cmd>Neorg keybind all<CR>", desc = "core", silent = true },
-    {
-      "<leader>nt",
-      function()
-        vim.api.nvim_command("Neorg toc right")
-        local width = vim.api.nvim_get_option("columns")
-        local new_width = math.floor(width * 0.35)
-        vim.api.nvim_command("vertical resize " .. new_width)
-      end,
-      desc = "Neorg tag",
-      silent = true,
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    build = ":Neorg sync-parsers",
+    keys = {
+      { "<leader>nl", "<cmd>Neorg<CR>", desc = "list", silent = true },
+      { "<leader>nk", "<cmd>Neorg keybind all<CR>", desc = "core", silent = true },
+      {
+        "<leader>nt",
+        function()
+          vim.api.nvim_command("Neorg toc right")
+          local width = vim.api.nvim_get_option("columns")
+          local new_width = math.floor(width * 0.35)
+          vim.api.nvim_command("vertical resize " .. new_width)
+        end,
+        desc = "Neorg tag",
+        silent = true,
+      },
     },
-  },
-  opts = {
-    load = {
-      -- ["core.defaults"] = {}, -- Loads default behaviour
-      ["core.itero"] = {},
-      ["core.promo"] = {},
-      ["core.integrations.treesitter"] = {},
-      -- ["core.integrations.telescope"] = {},
-      -- 在缓冲区编写代码块
-      -- ["core.looking-glass"] = {},
-      -- 自动缩进处理
-      ["core.norg.esupports.hop"] = {},
-      ["core.norg.esupports.indent"] = {},
-      -- 元数据
-      ["core.norg.esupports.metagen"] = {},
-      -- 生成索引文件
-      ["core.norg.qol.toc"] = {},
-      -- 日记
-      ["core.norg.journal"] = {
-        config = {
-          strategy = "flat",
-        },
-      },
-      ["core.norg.completion"] = {
-        config = {
-          engine = "nvim-cmp",
-        },
-      },
-      -- Adds pretty icons to your documents
-      ["core.norg.dirman"] = { -- Manages Neorg workspaces
-        config = {
-          workspaces = {
-            default = "~/Documents/neorg",
+    opts = {
+      load = {
+        -- ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.itero"] = {},
+        ["core.promo"] = {},
+        ["core.integrations.treesitter"] = {},
+        -- ["core.integrations.telescope"] = {},
+        -- 在缓冲区编写代码块
+        -- ["core.looking-glass"] = {},
+        -- 自动缩进处理
+        ["core.esupports.hop"] = {},
+        ["core.esupports.indent"] = {},
+        -- 元数据
+        ["core.esupports.metagen"] = {},
+        -- 生成索引文件
+        ["core.qol.toc"] = {},
+        -- 日记
+        ["core.journal"] = {
+          config = {
+            strategy = "flat",
           },
         },
-      },
-      -- todo
-      ["core.norg.qol.todo_items"] = {},
-      -- 美化
-      ["core.norg.concealer"] = {
-        config = {
-          folds = true,
-          icon_preset = "varied",
-          dim_code_blocks = {
-            conceal = false,
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
           },
-          icons = {
-            todo = {
-              done = { icon = " " },
-              pending = { icon = " " },
-              undone = { icon = " " },
-              uncertain = { icon = " " },
-              on_hold = { icon = " " },
-              cancelled = { icon = " " },
-              recurring = { icon = "↺" },
-              urgent = { icon = "⚠ " },
+        },
+        -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              default = "~/Documents/neorg",
             },
           },
         },
-      },
-      ["core.keybinds"] = {
-        config = {
-          default_keybinds = false,
-          hook = function(keybinds)
-            keybinds.map("norg", "n", "<Tab>", "za")
-            keybinds.map_event("norg", "n", "gtu", task_command("task_undone"), { desc = "未完成" })
-            keybinds.map_event("norg", "n", "gtp", task_command("task_pending"), { desc = "未决定" })
-            keybinds.map_event("norg", "n", "gtd", task_command("task_done"), { desc = "已完成" })
-            keybinds.map_event("norg", "n", "gth", task_command("task_on_hold"), { desc = "暂停" })
-            keybinds.map_event("norg", "n", "gtc", task_command("task_cancelled"), { desc = "取消" })
-            keybinds.map_event("norg", "n", "gtr", task_command("task_recurring"), { desc = "重复" })
-            keybinds.map_event("norg", "n", "gti", task_command("task_important"), { desc = "重要" })
-            keybinds.map_event_to_mode("norg", {
-              n = {
-                { "<leader>n<space>", "core.norg.qol.todo_items.todo.task_cycle", desc = "todo" },
-                { "J", "core.integrations.treesitter.next.heading" },
-                { "K", "core.integrations.treesitter.previous.heading" },
-                { "gd", "core.norg.esupports.hop.hop-link" },
-                { "gj", "core.integrations.treesitter.next.link" },
-                { "gk", "core.integrations.treesitter.previous.link" },
-                { ">", "core.promo.promote", "nested" },
-                { "<", "core.promo.demote", "nested" },
+        -- todo
+        ["core.qol.todo_items"] = {},
+        -- 美化
+        ["core.concealer"] = {
+          config = {
+            folds = true,
+            icon_preset = "varied",
+            dim_code_blocks = {
+              conceal = false,
+            },
+            icons = {
+              todo = {
+                done = { icon = " " },
+                pending = { icon = " " },
+                undone = { icon = " " },
+                uncertain = { icon = " " },
+                on_hold = { icon = " " },
+                cancelled = { icon = " " },
+                recurring = { icon = "↺" },
+                urgent = { icon = "⚠ " },
               },
-              i = {
-                { "<C-t>", "core.promo.promote" },
-                { "<C-d>", "core.promo.demote" },
-                { "<C-]>", "core.itero.next-iteration" },
-              },
-            }, {
-              silent = true,
-              noremap = true,
-            })
-          end,
+            },
+          },
+        },
+        ["core.keybinds"] = {
+          config = {
+            default_keybinds = false,
+            hook = function(keybinds)
+              keybinds.map("norg", "n", "<Tab>", "za")
+              keybinds.map_event("norg", "n", "gtu", task_command("task_undone"), { desc = "未完成" })
+              keybinds.map_event("norg", "n", "gtp", task_command("task_pending"), { desc = "未决定" })
+              keybinds.map_event("norg", "n", "gtd", task_command("task_done"), { desc = "已完成" })
+              keybinds.map_event("norg", "n", "gth", task_command("task_on_hold"), { desc = "暂停" })
+              keybinds.map_event("norg", "n", "gtc", task_command("task_cancelled"), { desc = "取消" })
+              keybinds.map_event("norg", "n", "gtr", task_command("task_recurring"), { desc = "重复" })
+              keybinds.map_event("norg", "n", "gti", task_command("task_important"), { desc = "重要" })
+              keybinds.map_event_to_mode("norg", {
+                n = {
+                  { "<leader>n<space>", "core.qol.todo_items.todo.task_cycle", desc = "todo" },
+                  { "J", "core.integrations.treesitter.next.heading" },
+                  { "K", "core.integrations.treesitter.previous.heading" },
+                  { "gd", "core.esupports.hop.hop-link" },
+                  { "gj", "core.integrations.treesitter.next.link" },
+                  { "gk", "core.integrations.treesitter.previous.link" },
+                  { ">", "core.promo.promote", "nested" },
+                  { "<", "core.promo.demote", "nested" },
+                },
+                i = {
+                  { "<C-t>", "core.promo.promote" },
+                  { "<C-d>", "core.promo.demote" },
+                  { "<C-]>", "core.itero.next-iteration" },
+                },
+              }, {
+                silent = true,
+                noremap = true,
+              })
+            end,
+          },
         },
       },
     },
-  },
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+    },
   },
 }
