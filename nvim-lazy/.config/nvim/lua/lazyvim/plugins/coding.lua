b1998a6750c6ce700ecd82287ad15a893809fb2e
yzl178me <1403341393@qq.com>
2023-07-01 15:18:33 +0800

    fix: prohibit luasnip

--- nvim-lazy/.config/nvim/lua/lazyvim/plugins/coding.lua
+++ nvim-lazy/.config/nvim/lua/lazyvim/plugins/coding.lua
@@ -86,8 +86,9 @@ return {
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
+            -- 跳转片段使用 没有片段会乱跳转
+            -- elseif luasnip.expand_or_jumpable() then
+            --   luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -96,8 +97,6 @@ return {
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
             else
               fallback()
             end
