b3d1370792af0ffdb2c877a8fa5e92d8f70eed4d
yzl178me <1403341393@qq.com>
2023-07-17 11:02:10 +0800

    feat: 安装最新懒猫云客户端jnbf

---
+++ script/install_lzc_client.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import re
+import requests
+
+url = "https://dl.lazycat.cloud/client/android/apk/"
+
+
+def get_latest_apk_url():
+    text = requests.get(url).text
+    # 定义正则表达式模式
+    pattern = r"v\d+\.\d+\.\d+.\d+"
+
+    # 在文本中查找匹配的版本号
+    versions = re.findall(pattern, text)
+    versions = tuple(map(lambda s: s.replace("-", "."), versions))
+
+    # 将版本号转换为数字并找到最大值
+    max_version = max(
+        map(lambda s: tuple(map(int, s[1:].split('.'))), versions))
+
+    # 将最大版本号转换回字符串格式
+    latest_version = 'v' + '.'.join(map(str, max_version))
+
+    # 在原始文本中查找最新版本的apk文件名
+    latest_apk = re.search(
+        f"lzc-client-android-{latest_version}-[a-z0-9]+\\.apk", text).group()
+    return url + latest_apk
+
+
+def get_testing_apk_url():
+    return "https://dl.lazycat.cloud/client/android/lzc-client-android.testing.apk"
+
+
+def get_stable_apk_url():
+    return "https://dl.lazycat.cloud/client/android/lzc-client-android.apk"
+
+
+filename = "lzc-client-android-dev.apk"
+
+apk_url = ""
+
+if len(sys.argv) >= 2:
+    if sys.argv[1] == "test":
+        apk_url = get_testing_apk_url()
+    elif sys.argv[1] == "stable":
+        apk_url = get_stable_apk_url()
+else:
+    apk_url = get_latest_apk_url()
+
+os.system(f"wget {apk_url} -O {filename}")
+#
+os.system(f"adb install {filename}")
+os.system(f"rm {filename}")
